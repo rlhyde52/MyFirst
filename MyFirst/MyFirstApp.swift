14d728af73f8ab2186c82797ed07b29aaf500a30
R Hyde <rlhyde@icloud.com>
2023-08-09 18:02:09 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Robert L. Hyde on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
